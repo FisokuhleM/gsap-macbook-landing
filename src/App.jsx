078a09fe0b6be20cd7ff72f670a0444788303e2f
Fisokuhle Mkhize <fisosingulvrity.mkhize@gmail.com>
2025-10-27 16:49:20 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import NavBar from './components/navbar.jsx'
+const App = () => {
+  return (
+    <main><NavBar /></main>
+  )
+}
+
+export default App
---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+export const navLinks = [
+    {label: 'Store'},
+    {label: 'Mac'},
+    {label: 'iPhone'},
+    {label: 'Mac'},
+    {label: 'Watch'},
+    {label: 'Vision'},
+    {label: 'AirPods'}
+]
